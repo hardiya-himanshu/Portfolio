204791c6b3999c95c0add3d9b50b2eafc8bb4a27
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2023-07-01 00:28:26 +0530

    Responsive completed except footer

---
+++ script.js
@@ -0,0 +1,24 @@
+// let menu = document.querySelector('.menu_icon');
+// let nav = document.querySelector('.hidden_nav');
+
+
+// menu.addEventListener("click", function(){
+//     nav.style.display="flex";
+// });
+
+function demoDisplay() {
+    if( document.querySelector('.hidden_nav').style.display =="flex"){
+        document.querySelector('.hidden_nav').style.display = "none";
+     }
+     else
+     {
+        document.querySelector('.hidden_nav').style.display = "flex";
+    }
+     
+  }
+
+
+
+
+
+
